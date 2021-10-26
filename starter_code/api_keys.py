43e674ddde564fe7bead148168cffc667048c593
Donterrius Daniels <85965807+Aether1906@users.noreply.github.com>
2021-10-26 12:17:57 -0400

    update

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "557460c78dc30d4522a8d6eb711b592a"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDFIxvsaRCjNMJ7Rxo3X37WukBZS6h-I74"
